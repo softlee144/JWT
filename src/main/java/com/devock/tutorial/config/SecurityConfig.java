0db4e91bdd8b8485719e54159c5ec7fab960b61e
softlee144 <ockjungbong@gmail.com>
2024-07-10 11:58:20 +0900

    JWT 코드, 시큐리티 설정 추가

--- src/main/java/com/devock/tutorial/config/SecurityConfig.java
+++ src/main/java/com/devock/tutorial/config/SecurityConfig.java
@@ -1,34 +1,70 @@
 package com.devock.tutorial.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.RequiredArgsConstructor;
+import com.devock.tutorial.jwt.JwtAccessDeniedHandler;
+import com.devock.tutorial.jwt.JwtAuthenticationEntryPoint;
+import com.devock.tutorial.jwt.JwtSecurityConfig;
+import com.devock.tutorial.jwt.TokenProvider;
 
-@EnableWebSecurity // 기본적인 Web 보안을 활성화 어노테이션
+@EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers("/h2-console/**", "/favicon.ico");
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
-                .authorizeHttpRequests()
-                .requestMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated(); // 나머지 API 는 전부 인증 필요
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf(AbstractHttpConfigurer::disable)
 
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint))
+
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        // token을 받는 API와 회원가입을 위한 API는 둘 다 토큰이 없는 상태에서 요청되므로 허용해준다.
+                        .requestMatchers("/api/hello", "/api/authenticate", "/api/signup").permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .anyRequest().authenticated())
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // enable h2-console
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+
+                .with(new JwtSecurityConfig(tokenProvider), customizer -> {
+                });
         return http.build();
     }
-
 }
